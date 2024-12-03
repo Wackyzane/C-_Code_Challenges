6b19b1ca6fdeb27cd1a8db455683e32028592641
wackyzane <44377480+wackyzane@users.noreply.github.com>
2024-12-02 20:38:38 -0700

    Adding Boolean to String File

---
+++ Scripts/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
+
+// Coding Challenge to turn Bollean into Strings
+public static void helloWorld
+{
+
+}
